d90d61fec9bfaf14600bf0bc7a6acf5a24c41a66
K <x00.x7f@gmail.com>
2016-11-01 16:07:23 +0900

    Added RidgeRegression

--- experiment/regression.py
+++ experiment/regression.py
@@ -16,3 +16,18 @@ class LinearRegression:
         # Ordinary Least Squares (OLS)
         ols = (error ** 2).sum()
         return ols
+
+
+# http://ie.u-ryukyu.ac.jp/~tnal/2016/info4/dm/2016info4dm-w5.pdf
+class RidgeRegression(LinearRegression):
+    alpha = None
+
+    def __init__(self,  alpha=0.1):
+        self.alpha = alpha
+
+    def fit(self, input, output):
+        xTx = np.dot(input.T, input)
+        I = np.eye(len(xTx))
+        # self.theta = np.dot(np.dot(np.linalg.inv(xTx + self.alpha * I), input.T),output)
+        self.theta =np.dot(np.linalg.solve(xTx + self.alpha * I, input.T), output)
+
